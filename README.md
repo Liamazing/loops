a7dfb50619ff9d6e3f6c39f25c80b7996e15b5d2
mgleeson <gleesonm@wustl.edu>
2017-12-05 19:12:45 -0600

    loop-server.js and README.MD

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Loop Site (Creative Project)
 # Matt Gleeson and Liam Osler
 
-Description: Our idea is to make a website where you are presented with a number of beats (probably 8) where you can select any of the diatonic notes from the scale and 
+Description: Our idea is to make a website where you are presented with a number of beats (probably 8) where you can select any of the diatonic notes from the scale and
 
 Total 95pts
 
@@ -12,25 +12,25 @@ Total 95pts
   5pts Users can select multiple notes on each beat (chords).
 
   5pts Users can change the tempo.
-  
+
   5pts Users can pause and play the loop.
 
   10pts Users can transpose their loops to a different key.
 
 25pts Other Functionality
-  
+
   10pts Users can save their loops, and get a shareable link to their loop.
 
   10pts The sound is generated through the use of the Web Audio API.
 
     https://css-tricks.com/introduction-web-audio-api/
-    
+
   5pts The note grid is generated and handled through the use of React.
-  
+
     https://reactjs.org/tutorial/tutorial.html
 
 10pts Usability
-  
+
 
   5pts Site is visually appealing.
 
@@ -39,9 +39,12 @@ Total 95pts
 5pts Best Practices
 
   3pts Code is well formatted and commented throughout
-  
+
   2pts Code passes the html validator
 
-20pts Creative Portion 
+20pts Creative Portion
 
 5pts Rubric was submitted on time (obv)
+
+Creative Portion Description:
+For our creative portion, we added a volume control function, used express and socket.io to allow asynchronous saving of files, added the ability to transpose between the Major, Natural Minor, Harmonic Minor, and Melodic Minor of the key, added a clear grid function, and used delta timing to ensure that the application wouldn’t skip beats/play them out of time. Users can also use the spacebar to play/pause the loop instead of clicking on the play/pause button. Integrating React and designing the UI took a lot longer than we expected, and ended up consisting of about half of the project time. We also set up our server to be constantly hosted on our AWS instance without the need for repeatedly calling the node function.
--- loop-server.js
+++ loop-server.js
@@ -1,5 +1,5 @@
 /* https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#deployment */
-
+//First, we make sure that we have all required packages
 const path = require('path'),
       express = require('express'),
       app = express(),
@@ -9,27 +9,25 @@ const path = require('path'),
 
 var dataFromFile;
 var indexToSave;
+
 app.use(express.static(path.join(__dirname, 'build')));
 
+//extracts the requested loop's index from the URL, ensures that it's a number, and reads and stores the data from the corresponding .json file
 app.get('/:loopIndex', function (req, res) {
     console.log(req.params.loopIndex);
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
     var parsedquery = parseInt(req.params.loopIndex);
     if(isNaN(parsedquery)){
       dataFromFile=false;
-      console.log("FILE INDEX IS NOT A NUMBER!");
     }
     else{
-      var content;
       var filepath = '/home/losler/saved_loops/'+parsedquery+'.json';
-      console.log("Reading data from path:" + filepath);
       fs.readFile(filepath, function (err, data) {
         if (err) data = false;
-        console.log(data);
         dataFromFile=data;
+        if(data==false) console.log("File: " + parsedquery + " does not exist!");
+        else console.log("Sending file of index: " + parsedquery);
       });
-
-
     }
 });
 
@@ -37,30 +35,22 @@ server.listen(3456);
 
 var io = socketio.listen(server);
 io.sockets.on("connection", function(socket){
-	// This callback runs when a new Socket.IO connection is established.
+	// Checks whether the client tried to access a file, and sends a socket.emit message with the file data to the client side of javascript
     if(!dataFromFile){
       socket.emit("load_file", false);
     }
     else{
-      console.log("DATA FROM FILE: " + dataFromFile);
-      console.log("data parsed:" + JSON.parse(dataFromFile));
       socket.emit("load_file", JSON.parse(dataFromFile));
     }
-
-
+    //Recieves the 'save_loop' event message from the client, and saves the unparsed json data to a new .json file
     socket.on('save_loop', function(data) {
-  		// This callback runs when the server receives a new message from the client.
       var fileContent = data;
-
       var dir = '/home/losler/saved_loops/';
       fs.readdir(dir, (err, files) => {
-        console.log(files);
-        console.log(files.length);
-        console.log(typeof files.length);
         if(err) throw err;
         indexToSave=parseInt(files.length,10);
         var filepath = dir+indexToSave+'.json';
-        console.log("attempting to save to: " + filepath);
+        //attempts to write, returning false if unsuccessful or with the index of the file if successful
         fs.writeFile(filepath,fileContent,(err) => {
           if(err){
             socket.emit("save_msg",false);
